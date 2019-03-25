33ff21bdfc9dddb8c79bf17272b4f9a0c6d28b6c
timothy <sevendark@yeah.net>
2019-03-26 02:41:43 +0800

    add _site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "GitHub blog 的搭建之路",
         "excerpt":"jekyll bundle  ","categories": ["GitHub Pages"],
         "tags": ["jekyll","github"],
-        "url": "https://blog.sevendark.cn/github%20pages/post-first/",
+        "url": "http://localhost:4000/github%20pages/post-first/",
         "teaser":null},{
         "title": "Post Second",
         "excerpt":"wahaha  ","categories": [],
         "tags": [],
-        "url": "https://blog.sevendark.cn/post-second/",
+        "url": "http://localhost:4000/post-second/",
         "teaser":null}]
